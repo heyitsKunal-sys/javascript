a874e8b2ac1ae08df4d817bf6fdbeabd89d86c48
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-04-26 22:58:33 +0530

    commit

---
+++ 11. Insert and remove elements/script.js
@@ -0,0 +1,37 @@
+// document.querySelector(".box")
+// <div class=​"box">​ hey iam a box​</div>​
+// document.querySelector(".box").innerHTML
+// ' hey iam a box'
+// document.querySelector(".container").innerHTML
+// '\n        <div class="box"> hey iam a box</div>\n    '
+
+// document.querySelector(".container").innerText
+// 'hey iam a box'
+
+// document.querySelector(".container").outerHTML
+// <div class="container">\n        <div class="box"> hey iam a box</div>\n    </div>'
+
+// document.querySelector(".container").tagName
+// DIV
+
+// document.querySelector(".container").nodeName
+// 'DIV'
+
+// document.querySelector(".container").hidden
+// false
+
+// document.querySelector(".box").innerHTML = "hey iam kunal"
+// "hey iam kunal" pehle hey iam a box thi 
+
+// document.querySelector(".box").hasAttribute("style")
+// false 
+
+// document.querySelector(".box").getAttribute("style")
+// 'display: flex;'
+
+// document.querySelector(".box").setAttribute("style", "display : inline") change kr dega or set kr dega new attribute
+
+//  document.designMode = "on" kisi bhi website ko edit kr skte ha
+
+// document.querySelector(".box").datset
+//  createdBy : kunal
