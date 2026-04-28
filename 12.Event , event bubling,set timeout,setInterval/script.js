60eeb926f0c705c899227810c57711a804f9d309
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-04-28 19:44:50 +0530

    new commit

--- 12.Event , event bubling,set timeout,setInterval/script.js
+++ 12.Event , event bubling,set timeout,setInterval/script.js
@@ -1,5 +1,13 @@
-let btn  = document.getElementById("btn")
-button.addEventListener("click" , () => {
-    alert("i was clicked yayyyyy")
+let button = document.getElementById("btn")
 
-})
+// button.addEventListener("click" , ()=>{
+//     alert("i was clicked yayyyyy")
+//     // ek alert message aaajyega!!!!
+
+// })  
+
+button.addEventListener("click" , ()=>{ 
+    document.querySelector(".box").innerHTML = " <b>  hey you were clicked </b>"
+
+})  
+//  hey iam a button ki jgah hey you were clicked ayega
