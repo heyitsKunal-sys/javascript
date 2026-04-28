5fd225f12abb2936f472bd4e08d8b3a60fcec02a
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-04-28 19:56:13 +0530

    new commit

--- 12.Event , event bubling,set timeout,setInterval/script.js
+++ 12.Event , event bubling,set timeout,setInterval/script.js
@@ -1,13 +1,24 @@
 let button = document.getElementById("btn")
 
+
 // button.addEventListener("click" , ()=>{
 //     alert("i was clicked yayyyyy")
 //     // ek alert message aaajyega!!!!
 
 // })  
 
-button.addEventListener("click" , ()=>{ 
-    document.querySelector(".box").innerHTML = " <b>  hey you were clicked </b>"
+// List of all mouse events 
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
 
-})  
+
+// button.addEventListener("dblclick" , ()=>{ 
+//     document.querySelector(".box").innerHTML = " <b>  hey you were clicked </b>"
+
+// })  
 //  hey iam a button ki jgah hey you were clicked ayega
+
+button.addEventListener("contextmenu" , ()=>{ 
+    alert("han bhai kya scn")
+
+}) 
+// right click krne pe alert msg open ayega
