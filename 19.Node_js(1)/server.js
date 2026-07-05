010402eb69fb1b87efe46d7d7fafd65a870c3aa9
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-05 15:38:45 +0530

    commit

--- 19.Node_js(1)/server.js
+++ 19.Node_js(1)/server.js
@@ -1,1 +1,7 @@
-console.log("Hello, World! This is a simple Node.js server.");
+// console.log("Hello, World! This is a simple Node.js server.");
+var slugify = require('slugify');
+let a =slugify('some string') 
+// if you prefer something other than '_' as seperator
+
+const b =slugify('some string', '_')  //some string
+console.log(this.methodName = this.methodName.bind(this))
