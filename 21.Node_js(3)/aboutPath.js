387309789092f3efc0020840d2268cf2ce16d945
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-06 18:54:28 +0530

    commit

--- 21.Node_js(3)/aboutPath.js
+++ 21.Node_js(3)/aboutPath.js
@@ -4,3 +4,5 @@ let myPath = "K:\\javascript\\21.Node_js(3)\\Kunal.txt"
 console.log(path.extname(myPath)) //.txt
 console.log(path.dirname(myPath)) //K:\javascript\21.Node_js(3)
 console.log(path.basename(myPath)) //Kunal.txt
+
+console.log(path.join()) // this function can join 2 paths 
