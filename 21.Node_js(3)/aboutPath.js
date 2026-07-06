2373168bd611cf740d0cf995c9a829781e5e2cd3
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-06 18:53:40 +0530

    commit

--- 21.Node_js(3)/aboutPath.js
+++ 21.Node_js(3)/aboutPath.js
@@ -2,3 +2,5 @@ import path from 'path'
 
 let myPath = "K:\\javascript\\21.Node_js(3)\\Kunal.txt"
 console.log(path.extname(myPath)) //.txt
+console.log(path.dirname(myPath)) //K:\javascript\21.Node_js(3)
+console.log(path.basename(myPath)) //Kunal.txt
