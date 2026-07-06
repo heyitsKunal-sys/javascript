e8b39530e2663e0b6d5c56ca86cd4a37fd24fdf8
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-06 18:16:09 +0530

    commit

--- 21.Node_js(3)/main.js
+++ 21.Node_js(3)/main.js
@@ -1,7 +1,26 @@
+// CREATING A FILE:
+
 const fs = require("fs")
-console.log( fs)
+
+
+
 
 
 console.log("starting")
-fs.writeFileSync("kunal.txt" , "kunal is a good boy")
+// fs.writeFileSync("kunal.txt" , "kunal is a good boy")
+// console.log("ending")
+
+fs.writeFile("kunal2.txt", "hey iam here", ()=>{
+    console.log("done")
+    fs.readFile("kunal2.txt",(error ,data)=>{     //here we use callback
+        console.log(error , data)  //error aya null and <Buffer 68 65 79 20 69 61 6d 20 68 65 72 65>
+        //agr buffer ko pdna h to use data.toString()
+        console.log(error , data.toString())   // null hey iam here
+    })
+})
 console.log("ending")
+// output:starting
+// ending
+// done :ye bad me chlne k liye schedule hua  
+
+// writeFilesync prblm create kr skta h so use writeFile
