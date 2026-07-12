35ca30a4d2a7fd836d7d6eef5a0f8f89b49f1e14
Kunal11102002 <bhardwajkunal0111@gmail.com>
2026-07-12 22:18:48 +0530

    commit

---
+++ 23.EXPRESS.js(2)/main.js
@@ -0,0 +1,53 @@
+// RESPONSE , REQUEST AND ROUTERS !!!!!
+
+
+const express = require('express');
+const blog = require('./routes/blog');
+
+
+
+const app = express();
+const port = 8080;
+app.use('./blog',blog)
+
+
+// Respond to GET request on the root route
+app.get('/', (req, res) => {
+    console.log("its a get request")
+    res.send('GET request to the homepage');
+});
+
+// Respond to POST request on the root route
+app.post('/', (req, res) => {
+    console.log("hey post bhai")
+    res.send('hello brother');
+});
+
+// Respond to GET request on the /about route
+app.get('/about', (req, res) => {
+
+    res.send('About page');
+});
+
+// Catch all other routes
+// app.all('*', (req, res) => {
+//     res.status(404).send('404 - Page not found');
+// });
+
+app.get('/index', (req, res) => {
+    console.log("hey index")
+    res.sendFile('template/index.html', { root: __dirname });
+    // hey iam bold this is index.html
+    //  res.sendFile : we serve html through this
+});
+app.get('/api', (req, res) => {
+    res.json({ a: 10, b: 2, c: 3, d: 5 })
+})   //{"a":10,"b":2,"c":3,"d":5}
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// Postman is an API platform used to test, document, and debug your Express.js backend endpoints without needing a frontend user interface.
+// It allows developers to send HTTP requests(like GET, POST, PUT, DELETE) directly to a local or remote Express server and view the server's responses.
--- 24.express.js(3)/main.js
+++ 24.express.js(3)/main.js
@@ -1,53 +1,12 @@
-// RESPONSE , REQUEST AND ROUTERS !!!!!
-
 
 const express = require('express');
-const blog = require('./routes/blog');
-
-
-
 const app = express();
-const port = 8080;
-app.use('./blog',blog)
+const port = 3000;
 
-
-// Respond to GET request on the root route
 app.get('/', (req, res) => {
-    console.log("its a get request")
-    res.send('GET request to the homepage');
+  res.send('Hello World its me!');
 });
 
-// Respond to POST request on the root route
-app.post('/', (req, res) => {
-    console.log("hey post bhai")
-    res.send('hello brother');
-});
-
-// Respond to GET request on the /about route
-app.get('/about', (req, res) => {
-
-    res.send('About page');
-});
-
-// Catch all other routes
-// app.all('*', (req, res) => {
-//     res.status(404).send('404 - Page not found');
-// });
-
-app.get('/index', (req, res) => {
-    console.log("hey index")
-    res.sendFile('template/index.html', { root: __dirname });
-    // hey iam bold this is index.html
-    //  res.sendFile : we serve html through this
-});
-app.get('/api', (req, res) => {
-    res.json({ a: 10, b: 2, c: 3, d: 5 })
-})   //{"a":10,"b":2,"c":3,"d":5}
-
-// Start the server
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
-
-// Postman is an API platform used to test, document, and debug your Express.js backend endpoints without needing a frontend user interface.
-// It allows developers to send HTTP requests(like GET, POST, PUT, DELETE) directly to a local or remote Express server and view the server's responses.
